7eea3ae5e76e1bd949b9dd10dfbe053ebeef5e46
Luis de la Peña <luis.delapena@hotmail.es>
2019-11-22 10:36:45 -0600

    Adding new path for testin

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
   res.send('Hello World! this node server was started by openshift deploy ... it creates a service and assigned new route');
 });
 
+pp.get('/test', function (req, res) {
+  res.send('Calling diferent path into same context /  :P'>
+});
+
+
 app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 8080 mi cabrón jajajaj!');
 });
