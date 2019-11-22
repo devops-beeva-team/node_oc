e29aeda4af920aa419a9bcc9175d02e6b4d6e0c0
Luis de la Peña <luis.delapena@hotmail.es>
2019-11-22 10:15:46 -0600

    fixing port binding

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('Hello World! this node server was started by openshift deploy ... it creates a service and assigned new route');
 });
 
-app.listen(8080, function () {
+app.listen(5000, function () {
   console.log('Example app listening on port 8080 mi cabrón jajajaj!');
 });
 
